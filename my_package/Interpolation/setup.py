0d86aca24b9b42af75b9f45e1a6b7ed8d0997709
Stalgia Grigg <stalgia.grigg@gmail.com>
2019-04-18 16:55:56 -0700

    nvcc_args for K80 for use with Colab

--- my_package/Interpolation/setup.py
+++ my_package/Interpolation/setup.py
@@ -8,6 +8,8 @@ from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 cxx_args = ['-std=c++11']
 
 nvcc_args = [
+    '-gencode', 'arch=compute_35,code=sm_35',
+    '-gencode', 'arch=compute_37,code=sm_37',
     '-gencode', 'arch=compute_50,code=sm_50',
     '-gencode', 'arch=compute_52,code=sm_52',
     '-gencode', 'arch=compute_60,code=sm_60',
